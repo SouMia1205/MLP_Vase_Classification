5b670b7df1bb04e2ec73fb302b7b60522f6d0d23
SouMia1205 <laksoumia31@gmail.com>
2025-03-07 16:14:48 +0100

    Updated MLP training code

--- MLP.py
+++ MLP.py
@@ -2,28 +2,28 @@
 import numpy as np  
 
 
-class Retropropagation:
-    def __init__(self, p, couche_cachés,q):
-        self.p = p  # nombre d'entrées (sans le biais)
+class MLP:
+    def __init__(self, n_entrées, couche_cachés,n_sorties):
+        self.n_entrées = n_entrées  # nombre d'entrées (sans le biais)
         self.couche_cachés = couche_cachés  # Liste du nombre de neurones dans chaque couche cachée
-        self.q = q  # nombre de couches ( incluant la couche d'entrée et la couche de sortie)
+        self.n_sorties = n_sorties  # nombre de neurones de sortie.
         
         # Initialisation des poids et biais
         self.W = []  # Liste des matrices de poids
-        self.b = []  # biais
+        self.b = []  # Liste des biais
         
         # Couche d'entrée  à première couche cachée
-        self.W.append(np.random.randn(couche_cachés[0], p+1))
+        self.W.append(np.random.randn(couche_cachés[0], n_entrées))
         self.b.append(np.random.randn(couche_cachés[0], 1))
         
         # (nombre de neurones dans la couche suivante) x (nombre de neurones dans la couche actuelle + 1) (le +1 est pour le biais).
         for i in range (1, len(couche_cachés)):
-            self.W.append(np.random.randn(couche_cachés[i], couche_cachés[i-1]+1))
+            self.W.append(np.random.randn(couche_cachés[i], couche_cachés[i-1]))
             self.b.append(np.random.randn(couche_cachés[i], 1))
         
         # Dernière couche cachée à la couche de sortie
-        self.W.append(np.random.randn(1, couche_cachés[-1]+1))
-        self.b.append(np.random.randn(1, 1))
+        self.W.append(np.random.randn(1, couche_cachés[-1]))
+        self.b.append(np.random.randn(n_sorties, 1))
     
     
     # Fonction d'activation sigmoid
@@ -34,6 +34,13 @@ class Retropropagation:
     def derivé_sigmoid(self, x):
         return x * (1 - x)
     
-    # Fonction de propagation avant
-    
+    # Fonction de propagation vers l'avant
+    def forward(self, X):
+        """
+        Propagation vers l'avant à travers le réseau.
+        :X -- Données d'entrée (n_entrées, n_exemples)
+        :return -- Sortie du réseau aprés activation.
+        """
+        A = X  # Initialisation de la sortie de la couche d'entrée
+        
     
