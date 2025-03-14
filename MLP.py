48bfae9394c4c1472ae0d8dd244f0b456de4a8c9
SouMia1205 <laksoumia31@gmail.com>
2025-03-14 17:55:46 +0100

    retropropagation du gradient de l'erreur

--- MLP.py
+++ MLP.py
@@ -61,7 +61,8 @@ class MLP:
         activa, sortie_predite, Valeurs_Z = self.forward(X)
         # Calcul de l'erreur (prédite et réelles)
         erreur = valeurs_vrais - sortie_predite
-        
+        # Calcul du gradient de l'erreur
+        gradient = erreur * self.derivé_sigmoid(sortie_predite)
         
 
 
